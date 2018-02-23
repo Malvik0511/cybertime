e0723f0d49ec75d8fa3807ffac9176d540591ff3
vmaltsev <malvik0510@gmail.com>
2018-02-23 11:50:36 +0500

    function asyncProcessing work

--- asyncProcessing/async.js
+++ asyncProcessing/async.js
@@ -1,39 +1,60 @@
 window.onload = function(){
-	function deepEqual(elt, secondElt){
-		if (arguments.length !== 2) throw Error('function should take 2 arguments');
-		if (typeof elt === 'object'){
-			if (Array.isArray(elt)){
-				if (!Array.isArray(secondElt)) return false;
-				if (elt.length !== secondElt.length) return false;
-				for(var i = 0; i < elt.length; i++){					
-					if (typeof elt[i] === 'function') continue;
-					if (!deepEqual(elt[i], secondElt[i])) {
-						return false;
-					}
-				}
-				return true;
-			}
-			if (typeof secondElt !== 'object') return false;
-			for (var prop in elt){
-				if (!elt.hasOwnProperty(prop)) continue;
-				if (typeof prop === 'function') continue;
-				if (secondElt[prop] === null) return false;
-				if (!deepEqual(elt[prop], secondElt[prop])) return false;
-			}
-			for (var prop in secondElt){
-				if (!secondElt.hasOwnProperty(prop)) continue;
-				if (typeof prop === 'function') continue;
-				if (elt[prop] === null) return false;
-				if (!deepEqual(elt[prop], secondElt[prop])) return false;
-			}
-			return true
-		}		
-		if (elt !== secondElt) return false;
-		return true;
+	function launchInParallel (func, data, n, callback) {
+		console.dir(func)
+		var i = 0,	
+	  		count = 0,
+	  		len = data.length
+  		function iterratorAsync(){
+  			if (count > n) {
+  				setTimeout(iterratorAsync, 1);
+  				console.log('count = ' + count)
+  				//console.log( count + ' overflow')
+  			}
+  			else{
+  				count++;
+  				func.call(null, data[i], function(){
+	  				count--;
+	  				console.log('i = '+ i)
+	  				if (i >= len && count === 0){
+	  					callback();
+	  					return;
+	  				}
+	  			})
+	  			if (i < len){
+	  				console.log('!')
+	  				i++;
+	  				iterratorAsync()
+	  			}
+	  			
+  			}
+	  	}
+	  	iterratorAsync();
+
+  	// не более n функций должны исполняться в любой момент времени
+  	// FIXME
 	}
-	
-	var a= [1,2,3], b = [1,2,3]
-	console.log(deepEqual(a, b))
+
+
+
+
+
+
+
+
+
+
+
+
+function sleep (s, callback) {
+  setTimeout(callback, s * 1000)
+}
+
+const data = [1, 3, 2, 1, 1, 4, 2, 5]
+
+launchInParallel(sleep, data, 5, () => {
+  // должно завершиться через 6 секунд
+  console.log('done')
+})
 	
 };
 
